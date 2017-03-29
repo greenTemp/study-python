6a7afdde043ceb0232543ab5826ac7ecbe67a165
nergal <nergal_lee@hotmail.com>
2017-03-29 22:55:30 +0800

    修改文件格式

--- ifelse.py
+++ ifelse.py
@@ -5,24 +5,24 @@ x = input()
 print 'please input y:'
 y = input()
 
-if x==0:
-    if y==0:
+if x == 0:
+    if y == 0:
         print 'origin'
-    elif y>0:
+    elif y > 0:
         print 'y+'
     else:
         print 'y-'
-elif x>0:
-    if y==0:
+elif x > 0:
+    if y == 0:
         print 'x+'
-    elif y>0:
+    elif y > 0:
         print '1'
     else:
         print '2'
 else:
-    if y==0:
+    if y == 0:
         print 'x-'
-    elif y>0:
+    elif y > 0:
         print '4'
     else:
         print '3'
